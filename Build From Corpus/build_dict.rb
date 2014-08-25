02fce91b3f7987f639514daf85be247b899cc1a8
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2014-08-25 10:22:55 +0200

    add dir for building from a legal corpus -- phase two

---
+++ Build From Corpus/build_dict.rb
@@ -0,0 +1,12 @@
+#! /usr/bin/env ruby
+
+# step 0: find defs
+# step 1: tokenize into word bags
+#   (document -> paragraphs(provisions, subprovisions) -> sentences -> words)
+#   drop trash words
+#   drop punctuation
+#   lowercase
+#   spell correct
+#   stemming
+# step 2: add meta-data to word bags
+
