4242ba8929f6e64c856ef733ebd725d5c26612c1
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2014-09-15 14:50:58 +0200

    step 1. count em

--- Build From Corpus/build_dict.rb
+++ Build From Corpus/build_dict.rb
@@ -10,3 +10,20 @@
 #   stemming
 # step 2: add meta-data to word bags
 
+if ARGV[0] == '--step-one'
+  collc = ARGV[1]
+  files = Dir.glob("#{collc}/*")
+  stops = File.readlines(File.join(File.dirname(__FILE__), 'stop_words.txt')).each{ |e| e.chomp! }
+  word_counts = Hash.new(0)
+
+  files.each do |file|
+    words = File.read(files[0]).split(' ').reject{ |e| ! e[/^[a-zA-Z]+/] }.map{ |e| e.gsub(/(\W|\d)/, "") }
+    words = words.each{ |e| e.downcase! }.reject{ |e| stops.include?(e) }
+    words.each {|w| word_counts[w.downcase] += 1 }
+    words = []
+  end
+
+  words = word_counts.to_a.sort_by{ |e| e[1] }.reverse.map{ |e| e.join(', ') }
+  writer = File.join(File.dirname(__FILE__), "#{collc}.txt")
+  File.open(writer, 'w'){|f| f.write(words.join("\n"))}
+end
